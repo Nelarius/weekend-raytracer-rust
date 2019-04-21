29f5e8e9609419821828c08bdfacca368f600472
Johann Muszynski <johann.muszynski@gmail.com>
2019-04-21 21:28:39 +0300

    Generate image on book cover

--- src/main.rs
+++ src/main.rs
@@ -63,58 +63,99 @@ fn main() {
 
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
-    // list of spheres
-    let spheres = vec![
+    // hitable spheres
+    let mut spheres = vec![
         Sphere::new(
-            Vec3::new(0.0, 0.0, -1.0),
-            0.5,
+            Vec3::new(0.0, -1000.0, -1.0),
+            1000.0,
             MaterialRecord::Lambertian(Lambertian {
-                albedo: Vec3::new(0.1, 0.2, 0.6),
+                albedo: Vec3::new(0.5, 0.5, 0.5),
             }),
         ),
         Sphere::new(
-            Vec3::new(0.0, -100.5, -1.0),
-            100.0,
-            MaterialRecord::Lambertian(Lambertian {
-                albedo: Vec3::new(0.8, 0.8, 0.0),
+            Vec3::new(0.0, 1.0, 0.0),
+            1.0,
+            MaterialRecord::Dielectric(Dielectric {
+                refraction_index: 1.5,
             }),
         ),
         Sphere::new(
-            Vec3::new(1.0, 0.0, -1.0),
-            0.5,
+            Vec3::new(-4.0, 1.0, 0.0),
+            1.0,
+            MaterialRecord::Lambertian(Lambertian {
+                albedo: Vec3::new(0.4, 0.2, 0.1),
+            }),
+        ),
+        Sphere::new(
+            Vec3::new(4.0, 1.0, 0.0),
+            1.0,
             MaterialRecord::Metal(Metal {
-                albedo: Vec3::new(0.8, 0.6, 0.2),
-                fuzz: 0.2,
-            }),
-        ),
-        Sphere::new(
-            Vec3::new(-1.0, 0.0, -1.0),
-            0.5,
-            MaterialRecord::Dielectric(Dielectric {
-                refraction_index: 1.5,
-            }),
-        ),
-        Sphere::new(
-            Vec3::new(-1.0, 0.0, -1.0),
-            -0.48,
-            MaterialRecord::Dielectric(Dielectric {
-                refraction_index: 1.5,
+                albedo: Vec3::new(0.7, 0.6, 0.5),
+                fuzz: 0.0,
             }),
         ),
     ];
 
-    let lookfrom = Vec3::new(-2.0, 1.0, 1.0);
-    let lookat = Vec3::new(0.0, 0.0, -1.0);
+    let sphere_offset = Vec3::new(4.0, 0.2, 0.0);
+    for a in -5..5 {
+        for b in -5..5 {
+            let a = a as f32;
+            let b = b as f32;
+            let center = Vec3::new(a + 0.9 * random::<f32>(), 0.2, b + 0.9 * random::<f32>());
+            let choose_mat = random::<f32>();
+            if (center - sphere_offset).length() > 0.9 {
+                if choose_mat < 0.8 {
+                    // diffuse
+                    spheres.push(Sphere::new(
+                        center,
+                        0.2,
+                        MaterialRecord::Lambertian(Lambertian {
+                            albedo: Vec3::new(
+                                random::<f32>() * random::<f32>(),
+                                random::<f32>() * random::<f32>(),
+                                random::<f32>() * random::<f32>(),
+                            ),
+                        }),
+                    ));
+                } else if choose_mat < 0.95 {
+                    // metal
+                    spheres.push(Sphere::new(
+                        center,
+                        0.2,
+                        MaterialRecord::Metal(Metal {
+                            albedo: Vec3::new(
+                                random::<f32>() * random::<f32>(),
+                                random::<f32>() * random::<f32>(),
+                                random::<f32>() * random::<f32>(),
+                            ),
+                            fuzz: 0.5 * random::<f32>(),
+                        }),
+                    ))
+                } else {
+                    spheres.push(Sphere::new(
+                        center,
+                        0.2,
+                        MaterialRecord::Dielectric(Dielectric {
+                            refraction_index: 1.5,
+                        }),
+                    ));
+                }
+            }
+        }
+    }
+
+    let lookfrom = Vec3::new(16.0, 2.0, 4.0);
+    let lookat = Vec3::new(0.0, 0.0, 0.0);
     let aspect_ratio = (WIDTH as f32) / (HEIGHT as f32);
     // let camera = Camera::new(90.0, aspect_ratio);
     let camera = Camera::new(
         lookfrom,
         lookat,
         Vec3::new(0.0, 1.0, 0.0),     // vup
-        30.0,                         // vfov
+        15.0,                         // vfov
         aspect_ratio,                 // aspect ratio
-        1.0,                          // aperture
-        (lookfrom - lookat).length(), // focust distance
+        0.2,                          // aperture
+        (lookfrom - lookat).length(), // focus distance
     );
 
     let world = World::new(spheres);
