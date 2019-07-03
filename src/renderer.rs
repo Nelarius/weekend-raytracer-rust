7907845becbcfcbc176f68e163f5a9e12ad0f288
jake <jh@discordapp.com>
2019-07-02 23:05:41 -0700

    use map init

--- src/renderer.rs
+++ src/renderer.rs
@@ -30,23 +30,25 @@ fn to_bgra(r: u32, g: u32, b: u32) -> u32 {
 pub fn render(width: usize, height: usize, camera: Camera, world: World) -> Vec<u32> {
     (0..width * height)
         .into_par_iter()
-        .map(|screen_pos| {
-            let mut rng = thread_rng();
-            let mut c = Vec3::new(0.0, 0.0, 0.0);
-            let i = height - 1 - screen_pos / width;
-            let j = screen_pos % width;
-            for _ in 0..NUM_SAMPLES {
-                let u = ((j as f32) + rng.gen::<f32>()) / (width as f32);
-                let v = ((i as f32) + rng.gen::<f32>()) / (height as f32);
-                let r = camera.make_ray(&mut rng, u, v);
-                c += color(r, &world, &mut rng, 0);
-            }
-            c = (1.0 / NUM_SAMPLES as f32) * c;
-            let ir = (255.99 * c.x.sqrt()) as u32;
-            let ig = (255.99 * c.y.sqrt()) as u32;
-            let ib = (255.99 * c.z.sqrt()) as u32;
+        .map_init(
+            || thread_rng(),
+            |mut rng, screen_pos| {
+                let mut c = Vec3::new(0.0, 0.0, 0.0);
+                let i = height - 1 - screen_pos / width;
+                let j = screen_pos % width;
+                for _ in 0..NUM_SAMPLES {
+                    let u = ((j as f32) + rng.gen::<f32>()) / (width as f32);
+                    let v = ((i as f32) + rng.gen::<f32>()) / (height as f32);
+                    let r = camera.make_ray(&mut rng, u, v);
+                    c += color(r, &world, &mut rng, 0);
+                }
+                c = (1.0 / NUM_SAMPLES as f32) * c;
+                let ir = (255.99 * c.x.sqrt()) as u32;
+                let ig = (255.99 * c.y.sqrt()) as u32;
+                let ib = (255.99 * c.z.sqrt()) as u32;
 
-            to_bgra(ir, ig, ib)
-        })
+                to_bgra(ir, ig, ib)
+            },
+        )
         .collect()
 }
